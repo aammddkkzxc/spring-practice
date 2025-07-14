e5aa3e325d956a8e4aef3b6277cf48e0cf25a3d6
aammddkkzxc <ideal0427@naver.com>
2025-07-14 21:28:20 +0900

    mvc1 setup

---
+++ mvc1-1/src/test/java/servlet/mvc11/Mvc11ApplicationTests.java
@@ -0,0 +1,13 @@
+package servlet.mvc11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
